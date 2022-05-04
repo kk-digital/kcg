43af7f366b5dd7adf9028b1073f02199745fe4fa
yolamu <32828093+yolamu@users.noreply.github.com>
2022-05-04 20:46:29 +0800

    test grid

---
+++ Assets/GridBuildingSystem/Scripts/PlacedObjectTypeSO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class PlacedObjectTypeSO : ScriptableObject {
+
+    public static Dir GetNextDir(Dir dir) {
+        switch (dir) {
+            default:
+            case Dir.Down:      return Dir.Left;
+            case Dir.Left:      return Dir.Up;
+            case Dir.Up:        return Dir.Right;
+            case Dir.Right:     return Dir.Down;
+        }
+    }
+
+    public enum Dir {
+        Down,
+        Left,
+        Up,
+        Right,
+    }
+
+    public string nameString;
+    public Transform prefab;
+    public Transform visual;
+    public int width;
+    public int height;
+
+
+}
