cf994c072853b88aa917c35172b4528a7e08aeba
yolamu <32828093+yolamu@users.noreply.github.com>
2022-05-13 22:42:51 +0800

    grid test

---
+++ Assets/gridECS/Extensions/PoolExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Entitas;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PoolExtensions {
+
+    public static float GetDistanceBetweenNodes(Entity a, Entity b)
+    {
+        float result = 1;
+
+        return result;
+    }
+
+    public static List<Entity> RetracePath(Entity startNode, Entity endNode)
+    {
+        List<Entity> path = new List<Entity>();
+        Entity currentNode = endNode;
+        while(currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.node.parent;
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+}
