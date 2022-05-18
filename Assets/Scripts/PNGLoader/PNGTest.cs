cd1cb06240044ed7a9ce6a421efb0c164358378d
Gairus Niel <nielstupidity@gmail.com>
2022-05-18 14:29:33 +0800

    Update PNGTest.cs

--- Assets/Scripts/PNGLoader/PNGTest.cs
+++ Assets/Scripts/PNGLoader/PNGTest.cs
@@ -7,15 +7,13 @@ namespace PNGLoader
 {
     public class PNGTest : MonoBehaviour
     {
-        void Awake()
-        {
-            SceneManager.Instance.Register(this.GetType().Name);
-        }
         
         private void Start() 
         {
             PngLoaderManager.InitStage1();
             PngLoaderManager.DebugImageDatas();
+
+            //SceneManager.Instance.Register(this.GetType().Name);
         }
         
     }
