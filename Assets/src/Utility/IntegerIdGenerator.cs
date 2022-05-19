93c10d7bf7221bda0a03d1d7bdef07e6c373775b
Wylan Osorio <wylanosorio@gmail.com>
2022-05-20 00:26:36 +0800

    id generator

---
+++ Assets/src/Utility/IntegerIdGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.src.Utility
+{
+    public class IntegerIdGenerator
+    {
+        private readonly Random random = new Random();
+        private readonly List<int> randomList = new List<int>();
+
+        public int NewId()
+        {
+            int newId = random.Next(0, 1000);
+
+            while (randomList.Contains(newId))
+                newId = random.Next(0, 1000);
+
+            randomList.Add(newId);
+
+            return newId;
+        }
+    }
+}
