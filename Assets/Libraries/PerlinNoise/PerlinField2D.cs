f7ca3ab2b2e7d3ef514f528662f6b5ab379e303c
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-05-19 13:20:59 +0200

    Ported Perlin Noise 2D from C++

---
+++ Assets/Libraries/PerlinNoise/PerlinField2D.cs
@@ -0,0 +1,113 @@
+using PRNG;
+using System;
+
+namespace PerlinNoise {
+    public class PerlinField2D {
+        static public int[] primes =  {
+             2,  3,  5,  7, 11,
+            13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47,
+            53, 59, 61, 67, 71
+        };
+        
+        static private float dot(int gi, float x, float y, float z) {
+            int[,] g3 = {
+                {1, 1, 0}, {-1,  1, 0}, {1, -1,  0}, {-1, -1,  0},
+                {1, 0, 1}, {-1,  0, 1}, {1,  0, -1}, {-1,  0, -1},
+                {0, 1, 1}, { 0, -1, 1}, {0,  1, -1}, { 0, -1, -1}
+            };
+        
+            return g3[gi, 0] * x
+                 + g3[gi, 1] * y
+                 + g3[gi, 2] * z;
+        }
+
+        static private int fast_floor(float value) { return (int)(value >= 0 ? (int)value : (int)value - 1); }
+        static private float mix(float a, float b, float t) { return a + t * (b - a); }
+        static private float fade(float t) { return t * t * t * (t * (t * 6 - 15) + 10); }
+
+        public  byte[] ga;
+        public float[] grad;
+
+        public int ssize;
+        public int xsize;
+
+        public int grad_max;
+
+        public void init(int _xsize, int _grad_max) {
+            if(ga != null || grad != null) return;
+
+            xsize = _xsize;
+            ssize = xsize * xsize;
+            grad_max = _grad_max;
+
+            ga = new byte[ssize];
+            grad = new float[2 * grad_max];
+
+            generate_gradient_array();
+            generate_gradient_vectors();
+        }
+
+        public void generate_gradient_array() {
+            for(int i = 0; i < ssize; i++)
+                ga[i] = (byte)(Mt19937.genrand_int32() % (ulong)grad_max);
+        }
+
+        public void generate_gradient_vectors() {
+            for (int i = 0; i < grad_max; i++) {
+                float t = 6.28318531f * i * (1.0f / (float)grad_max);
+                float x = MathF.Sin(t);
+                float y = MathF.Cos(t);
+
+                grad[2 * i + 0] = x;
+                grad[2 * i + 1] = y;
+            }
+        }
+
+        public int get_gradient(int x, int y) {
+            x = x % xsize;
+            y = y % xsize;
+
+            return ga[x + y * xsize];
+        }
+
+        public float dot_grad(int index, float x, float y) {
+            return grad[2 * index + 0] * x
+                 + grad[2 * index + 1] * y;
+        }
+
+        public float _base(float x, float y) {
+            x *= xsize;
+            y *= xsize;
+
+            int X = fast_floor(x);
+            int Y = fast_floor(y);
+
+            x = x - X;
+            y = y - Y;
+
+            int gi00 = get_gradient(X + 0, Y + 0);
+            int gi01 = get_gradient(X + 0, Y + 1);
+            int gi10 = get_gradient(X + 1, Y + 0);
+            int gi11 = get_gradient(X + 1, Y + 1);
+
+            // Calculate noise contributions from each of the eight corners
+
+            float n00 = dot_grad(gi00, x, y);
+            float n10 = dot_grad(gi10, x - 1, y);
+            float n01 = dot_grad(gi01, x, y - 1);
+            float n11 = dot_grad(gi11, x - 1, y - 1);
+
+            // Compute the fade curve value for each of x, y, z
+
+            float u = fade(x);
+            float v = fade(y);
+
+            float nx00 = mix(n00, n10, u);
+            float nx10 = mix(n01, n11, u);
+            float nxy  = mix(nx00, nx10, v);
+
+            return nxy * MathF.Sqrt(2.0f); // -1 to 1
+        }
+    }
+}
---
+++ Assets/Libraries/PerlinNoise/PerlinOctave2D.cs
@@ -0,0 +1,214 @@
+using PRNG;
+using System;
+
+namespace PerlinNoise {
+    public class PerlinOctave2D {
+        public int octaves;
+        public PerlinField2D[] octave_array;
+
+        public float[] cache;
+        public float cache_persistence;
+        public ulong cache_seed;
+
+        private int map_dim_x;
+        private int map_dim_y;
+
+        private int runs;
+
+        private void _init(int _octaves, int _map_dim_x, int _map_dim_y, bool change_seed = true) {
+            runs = 0;
+            cache = null;
+            cache_persistence = 0.0f;
+            cache_seed = 0;
+
+            map_dim_x = _map_dim_x;
+            map_dim_y = _map_dim_y;
+
+            octaves = _octaves;
+
+            if(change_seed)
+                Mt19937.seed_twister((ulong)(new Random()).Next());
+
+            octave_array = new PerlinField2D[octaves];
+
+            cache = new float[(map_dim_x / 4) * (map_dim_y / 4)];
+
+            for(int i = 0; i < octaves; i++)
+                octave_array[i].init(PerlinField2D.primes[i + 1], 16);
+        }
+
+        public PerlinOctave2D(int octaves, int dim_x, int dim_y) { _init(octaves, dim_x, dim_y); }
+        public PerlinOctave2D(int octaves, int dim_x, int dim_y, bool change_seed) { _init(octaves, dim_x, dim_y, change_seed); }
+
+        public void set_persistence(float persistence) {
+            if(cache_persistence == persistence) return;
+            cache_persistence = persistence;
+            populate_cache(persistence);
+        }
+
+        public void set_param(float persistence, bool change_seed = true) {
+            bool update = false;
+
+            if(runs == 0) {
+                update = true;
+                if(change_seed) Mt19937.seed_twister(cache_seed = (ulong)(new Random()).Next());
+               
+                for(int i = 0; i < octaves; i++)
+                    octave_array[i].generate_gradient_array();
+            }
+
+            if(persistence != cache_persistence || update) 
+                populate_cache(cache_persistence = persistence);
+
+            runs++;
+        }
+
+        public void populate_cache(float persistence) {
+            int xmax = map_dim_x / 4;
+            int ymax = map_dim_y / 4;
+
+            float x, y;
+
+            for(int i = 0; i < xmax; i++)
+                for(int j = 0; j < ymax; j++) {
+                    x = i * 4.0f / (float)map_dim_x;
+                    y = j * 4.0f / (float)map_dim_y;
+
+                    cache[j * xmax + i] = sample(x, y, persistence);
+                }
+        }
+
+        
+        public float sample(float x, float y, float persistence) {
+            float p = 1.0f;
+            float tmp = 0.0f;
+
+            for(int i = 0; i < octaves; i++) {
+                tmp += octave_array[i]._base(x, y);
+                p *= persistence;
+            }
+
+            return tmp;
+        }
+
+        float sample2(float x, float y, float persistence) {
+            float p = 1.0f;
+            float tmp = 0.0f;
+
+            for(int i = 0; i < octaves; i++) {
+                Console.WriteLine("octave " + i + ": " + octave_array[i]._base(x, y));
+                tmp += p * octave_array[i]._base(x, y);
+                p *= persistence;
+            }
+            
+            Console.WriteLine("tmp = " + tmp + " x, y = " + x + ", " + y);
+            return tmp;
+        }
+        
+        public void save_octaves() {
+            float xresf = 1.0f / (float)map_dim_x;
+            float yresf = 1.0f / (float)map_dim_y;
+
+            float[] output = new float[map_dim_x * map_dim_y * octaves];
+
+            PerlinField2D m;
+            for(int k = 0; k < octaves; k++) {
+                m = octave_array[k];
+                int zoff = k * map_dim_x * map_dim_y;
+
+                for(int i = 0; i < map_dim_x; i++)
+                    for(int j = 0; j < map_dim_y; j++) {
+                        float x = i * xresf;
+                        float y = j * yresf;
+
+                        output[i + j * map_dim_y + zoff] = m._base(x, y);
+                    }
+            }
+
+            //save_perlin("octave_map_01", out, xres, yres*octaves);
+
+            for(int k = 0; k < octaves; k++)
+                for(int i = 0; i < map_dim_x; i++)
+                    for(int j = 0; j < map_dim_y; j++)
+                        output[i + j * map_dim_y + k * map_dim_x * map_dim_y] = 0.0f;
+
+            for(int k = 0; k < octaves; k++) 
+                for(int k2 = 0; k2 < k; k2++) {
+                    m = octave_array[k2];
+                    float p = 1.0f;
+                    int zoff = k * map_dim_x * map_dim_y;
+
+                    for(int i = 0; i < map_dim_x; i++)
+                        for(int j = 0; j < map_dim_y; j++) {
+                            float x = i * xresf;
+                            float y = j * yresf;
+
+                            output[i + j * map_dim_y + zoff] += p * m._base(x, y);
+                        }
+
+                    p *= 0.50f;
+                }
+
+
+            //save_png("octave_map_01", output, xres, yres*octaves);
+            //save_perlin("octave_map_02", output, xres, yres*octaves);
+        }
+
+        public void save_octaves2(int degree, string filename) {
+            int xres = 128;
+            int yres = 128;
+
+            float xresf = 1.0f / (float)xres;
+            float yresf = 1.0f / (float)yres;
+
+            float[] output = new float[xres * yres * octaves * degree];
+
+            int line_width = xres * degree;
+
+            for(int i = 0; i < xres * yres * octaves * degree; i++) output[i] = 0.0f;
+
+            PerlinField2D m;
+
+            for(int k = 0; k<octaves; k++) {
+                m = octave_array[k];
+                int zoff = k * yres * line_width;
+
+                for(int i = 0; i<xres; i++)
+                    for(int j = 0; j<yres; j++) {
+                        float x = i * xresf;
+                        float y = j * yresf;
+
+                        output[i+j * line_width + zoff] += m._base(x, y);
+                    }
+            }
+
+            for(int n = 1; n < degree; n++) {
+                float persistence = ((float)n + 1.0f) / (float)degree;
+
+                int xoff = n * xres;
+
+                for(int k = 0; k < octaves; k++) {
+                    int yoff = k * yres * line_width;
+                    float p = 1.0f;
+
+                    for(int k2 = 0; k2 <= k; k2++) {
+                        m = octave_array[k2];
+
+                        for(int i = 0; i < xres; i++)
+                            for(int j = 0; j < yres; j++) {
+                                float x = i * xresf;
+                                float y = j * yresf;
+
+                                output[(i + xoff) + (j * line_width + yoff)] += p * m._base(x, y);
+                            }
+
+                        p *= persistence;
+                    }
+                }
+
+            }
+            //save_png("octave_map_01", output, xres, yres*octaves);
+            //save_perlin(filename, output, xres*degree, yres*octaves);
+        }
+    }
+}
