a3537cdd3119897489185d0a4db82e629425706e
Choi <ridvansu@hotmail.com>
2022-05-20 13:42:36 +0300

    Pixel Perfect Camera: Alpha Blending Option Added #30
    Alpha Blending option added to grid's component, and some comments and edits on code. Issue #30

--- Assets/Scripts/PixelSnap.cs
+++ Assets/Scripts/PixelSnap.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
+//NEED SPRITE RENDERER COMPONENT TO WORK
 [ExecuteInEditMode]
 [RequireComponent(typeof(SpriteRenderer))]
 public class PixelSnap : MonoBehaviour
 {
+    // Sprite target
     private Sprite sprite;
+
+    // Acutal poisiton world space
     private Vector3 actualPosition;
+
+    // Option to restore position
     private bool shouldRestorePosition;
 
     // Use this for initialization
     void Start()
     {
+        // Assigning renderer
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
         if (renderer != null)
         {
@@ -23,10 +30,9 @@ public class PixelSnap : MonoBehaviour
         }
     }
 
-
+    // Doc: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnWillRenderObject.html
     void OnWillRenderObject()
     {
-        //Debug.Log("on will" + Camera.current);
         Camera cam = Camera.current;
         if (!cam)
             return;
