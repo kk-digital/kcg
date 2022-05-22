0532920d881ce42de264e8254d834787d606d1ab
Choi <ridvansu@hotmail.com>
2022-05-22 13:13:54 +0300

    OpenGL Support: GLSL Test Scene Added #85
    OpenGL, GLSL test scene added. 3 geometric shapes colored with GLSL shading. Issue #85

---
+++ Assets/Scripts/GLTestCube.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Enums;
+
+/* 
+    GLSL CUBE RENDERING TEST SCRIPT
+*/
+
+class GLTestCube : MonoBehaviour
+{
+
+    // Documentation: https://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html
+    private void Awake()
+    {
+        //Check if SceneManager even exists
+        if (SceneManager.Instance != null)
+        {
+            SceneManager.Instance.Register(this, SceneObjectType.SceneObjectTypeUtilityScript);
+        }
+    }
+
+    // Doc: https://docs.unity3d.com/ScriptReference/MonoBehaviour.LateUpdate.html
+    void LateUpdate()
+    {
+        // Rotating cube around Y axis
+        transform.Rotate(new Vector3(25f * Time.deltaTime, 25.0f * Time.deltaTime, 0f));
+    }
+}
