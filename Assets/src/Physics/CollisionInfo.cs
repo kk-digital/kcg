ae855cf748b16e731f06ddb84b39146b6a38e301
Anton Zhukov <anton.zhukov@nanoreality.com>
2022-05-23 17:50:17 +0300

    Moved Body, CollisionInfo to the Physics folder

---
+++ Assets/src/Physics/CollisionInfo.cs
@@ -0,0 +1,17 @@
+namespace Physics
+{
+    public struct CollisionInfo
+    {
+        public bool Above;
+        public bool Below;
+        public bool Left;
+        public bool Right;
+
+        public void Reset() => Above = Below = Left = Right = false;
+    
+        public bool Collided() => Above || Below || Left || Right;
+        public bool Horizontal() => Left || Right;
+        public bool Vertical() => Above || Below;
+        public bool VerticalAbove() => Above;
+    }
+}
