346e10bd34428ba6577dc69402640ef3e119d9a9
zoubirabde9a <74624149+zoubirabde9a@users.noreply.github.com>
2022-05-28 15:47:42 +0100

    sprite atlas implementation

--- Assets/Scripts/MapLoaderTestScript.cs
+++ Assets/Scripts/MapLoaderTestScript.cs
@@ -18,7 +18,7 @@ namespace PlanetTileMap.Unity
     // there will be things like rendering, collision, TileMap
     // that are not supposed to be here.
 
-    class TilesRenderer : MonoBehaviour
+    class MapLoaderTestScript : MonoBehaviour
     {
         //public string TileMap = "Moonbunker/Moon Bunker.tmx";
         [SerializeField] Material Material;
@@ -33,7 +33,6 @@ namespace PlanetTileMap.Unity
         RectangleBoundingBoxCollision Player;
         Vector2 PlayerPosition;
 
-        int PlayerSpriteID;
         int PlayerSprite2ID;
         const float TileSize = 1.0f;
 
@@ -44,41 +43,33 @@ namespace PlanetTileMap.Unity
 
         public void Start()
         {
+            // inittialize the sprites/ tiles ect..
             if (!InitTiles)
             {
+                // loads the sprites into memory
+                LoadSprites();
+
+                // creates the tiles properties
                 CreateDefaultTiles();
+
+                // creates collision stuff
                 CreateTestPlayer();
+
                 InitTiles = true;
             }
             // TODO(Mahdi): does not make sense to put them here
             // move them out ! 
-            InitStage1();
-            InitStage2();
-        }
-
-        //All memory allocations/setups go here
-        //File loading should not occur at this stage
-        public void InitStage1()
-        {
-            // todo: commented out the tmx stuff for now
-            /*FileLoader = new TmxImporter(Path.Combine(BaseDir, TileMap));
-            FileLoader.LoadStage1();*/
-        }
-
-        //Load settings from files and other init, that requires systems to be intialized
-        public void InitStage2()
-        {
- 
-
-            //TestDrawTiles();
             LateUpdate();
+        }  
 
 
-        }      
-
+        
         public void Update()
         {
-                       //remove all children MeshRenderer
+            // in here we will draw everything to the screen
+
+
+            //remove all children MeshRenderer
             foreach(var mr in GetComponentsInChildren<MeshRenderer>())
                 if (Application.isPlaying)
                     Destroy(mr.gameObject);
@@ -105,10 +96,17 @@ namespace PlanetTileMap.Unity
                 PlayerPosition.x -= speed * Time.deltaTime;
             }
 
+
+            // drawing the player to the screen
             PlayerCollidersTest();
+
+            // drawing the tile map to the screen
             DrawMapTest();
         }
 
+        // this function is used to draw a tile map to the screen
+        // it will only draw the tiles that are visible
+        // only works for orthographic mode
         void DrawMapTest()
         {
 
@@ -147,20 +145,32 @@ namespace PlanetTileMap.Unity
             }
         }
 
-
-        //NOTE(Mahdi): this is used to create some test tiles
-        // to make sure the system is working
-        public void CreateDefaultTiles()
+        // loading the sprite sheets and filling up the sprite atlas
+        public void LoadSprites()
         {
+
+            // load all the sprite sheets
             int MetalSlabsTileSheet = 
                         GameState.TileSpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\Tiles_metal_slabs\\Tiles_metal_slabs.png");
             int StoneBulkheads = 
                         GameState.TileSpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\tile_wallbase\\Tiles_stone_bulkheads.png");
             int TilesMoon = 
                         GameState.TileSpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\tiles_moon\\Tiles_Moon.png");
+            int PlayerTileSheet = 
+                        GameState.SpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\character\\character.png",
+                         32, 48);
+
+
+
+          // blit the player sprite sheet to the atlas.
+          // returns an id, we can use that for drawing
+           PlayerSprite2ID = GameState.SpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);;
+
+            //
 
 
             // creating the tiles
+            // we can add more properties to the tiles
             GameState.TileCreationApi.CreateTile(0);
             GameState.TileCreationApi.SetTileName("slab1");
             GameState.TileCreationApi.SetTileTexture16(MetalSlabsTileSheet, 0, 0);
@@ -202,6 +212,14 @@ namespace PlanetTileMap.Unity
             GameState.TileCreationApi.EndTile();
 
 
+
+        }
+
+        //NOTE(Mahdi): this is used to create some test tiles
+        // to make sure the system is working
+        public void CreateDefaultTiles()
+        {
+            
             // Generating the map
             int mapWidth = 128;
             int mapHeight = 128;
@@ -238,56 +256,44 @@ namespace PlanetTileMap.Unity
             }
 
         }
-
-
-      
+        
         void CreateTestPlayer()
         {
-            int PlayerTileSheet = 
-                        GameState.TileSpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\character\\character.png");
-
-            int PlayerTileSheet2 = 
-                        GameState.SpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\character\\character.png",
-                         32, 48);
-
-
-            PlayerSpriteID =  GameState.TileSpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);
-            PlayerSprite2ID =  GameState.SpriteAtlasManager.Blit(PlayerTileSheet2, 0, 0, 32, 48);;
-
             PlayerPosition = new Vector2(6, 6);
 
             // 1f - considered to be 32 pixel
-            Player = new RectangleBoundingBoxCollision(PlayerPosition - MapOffset, new Vector2(1f, 1f));
+            Player = new RectangleBoundingBoxCollision(PlayerPosition - MapOffset, new Vector2(1f, 48.0f / 32.0f));
         }
 
+
+        // draws the player to the screen
         void PlayerCollidersTest()
         {
-            Player = new RectangleBoundingBoxCollision(PlayerPosition - MapOffset, new Vector2(1f, 1f));
+            Player = new RectangleBoundingBoxCollision(PlayerPosition - MapOffset, new Vector2(1f, 48.0f / 32.0f));
             bool isCollidingBottom = Player.IsCollidingBottom(ref TileMap);
 
-            Debug.Log($"Player Bottom Collided: {isCollidingBottom}");
+            //Debug.Log($"Player Bottom Collided: {isCollidingBottom}");
 
 
-            byte[] spriteBytes = new Byte[32 * 32 * 4];
+            byte[] spriteBytes = new Byte[32 * 48 * 4];
 
             if (isCollidingBottom)
             {
-                GameState.TileSpriteAtlasManager.GetSpriteBytes(PlayerSpriteID, spriteBytes);
+                GameState.SpriteAtlasManager.GetSpriteBytes(PlayerSprite2ID, spriteBytes);
             }
             else
             {
-                 GameState.TileSpriteAtlasManager.GetSpriteBytes(PlayerSpriteID, spriteBytes);
+                 GameState.SpriteAtlasManager.GetSpriteBytes(PlayerSprite2ID, spriteBytes);
             }
 
-            DrawSprite(PlayerPosition.x, PlayerPosition.y, 1.0f, 1.0f, spriteBytes, 32, 32);
+            DrawSprite(PlayerPosition.x, PlayerPosition.y, 1.0f, 48.0f / 32.0f, spriteBytes, 32, 48);
 
         }
 
 
         public void LoadMap()
         {
-            InitStage1();
-            InitStage2();
+            LateUpdate();
         }
 
 
@@ -372,11 +378,7 @@ namespace PlanetTileMap.Unity
 
         private void LateUpdate()
         {
-         /*   var visibleRect = CalcVisibleRect();
 
-            //rebuild all layers for visible rect
-            foreach (var mb in meshBuildersByLayers)
-                mb.BuildMesh(visibleRect);*/
         }
 
         public struct R
@@ -404,6 +406,8 @@ namespace PlanetTileMap.Unity
             return visibleRect;
         }
 
+
+        // converts pixel to a unity Texture2D
         private Texture2D CreateTextureFromRGBA(byte[] rgba, int w, int h)
         {
 
@@ -481,12 +485,12 @@ namespace PlanetTileMap.Unity
     }
 
 #if UNITY_EDITOR
-    [CustomEditor(typeof(TilesRenderer))]
+    [CustomEditor(typeof(MapLoaderTestScript))]
     public class TerrainGeneratorEditor : Editor
     {
         public override void OnInspectorGUI()
         {
-            var myTarget = (TilesRenderer)target;
+            var myTarget = (MapLoaderTestScript)target;
 
             // Show default inspector property editor
             DrawDefaultInspector();
---
+++ Assets/Scripts/SpriteAtlasTest.cs
@@ -0,0 +1,244 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using Physics;
+using TileProperties;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+namespace PlanetTileMap.Unity
+{
+    //Note: TileMap should be mostly controlled by GameManager
+
+
+    //Note(Mahdi): we are just testing and making sure everything is working
+    // before we move things out of here
+    // there will be things like rendering, collision, TileMap
+    // that are not supposed to be here.
+
+    class SpriteAtlasTest : MonoBehaviour
+    {
+        //public string TileMap = "Moonbunker/Moon Bunker.tmx";
+        [SerializeField] Material Material;
+
+        public static string BaseDir => Application.streamingAssetsPath;
+
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<Vector3> verticies = new List<Vector3>();
+
+        Vector2 MapOffset = new Vector2(-3.0f, 4.0f);
+
+        static bool InitTiles = false;
+        
+
+        public void Start()
+        {
+            if (!InitTiles)
+            {
+                LoadSprites();
+                InitTiles = true;
+            }
+            // TODO(Mahdi): does not make sense to put them here
+            // move them out ! 
+            InitStage1();
+            InitStage2();
+        }
+
+        //All memory allocations/setups go here
+        //File loading should not occur at this stage
+        public void InitStage1()
+        {
+            // todo: commented out the tmx stuff for now
+            /*FileLoader = new TmxImporter(Path.Combine(BaseDir, TileMap));
+            FileLoader.LoadStage1();*/
+        }
+
+        //Load settings from files and other init, that requires systems to be intialized
+        public void InitStage2()
+        {
+
+            LateUpdate();
+        }      
+
+        public void Update()
+        {
+                       //remove all children MeshRenderer
+            foreach(var mr in GetComponentsInChildren<MeshRenderer>())
+                if (Application.isPlaying)
+                    Destroy(mr.gameObject);
+                else
+                    DestroyImmediate(mr.gameObject);
+
+            DrawSpriteAtlas();
+        }
+
+        // create the sprite atlas for testing purposes
+        public void LoadSprites()
+        {
+            // we load the sprite sheets here
+            int SomeObjectTileSheet = 
+                        GameState.SpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\Objects\\algaeTank1.png",
+                         32, 64);
+            int PlayerTileSheet = 
+                        GameState.SpriteLoader.GetSpriteSheetID("Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\character\\character.png",
+                         32, 48);
+
+
+            // bit the sprites into the sprite atlas
+            // we can blit the same sprite
+            // but its only for testing purpose
+            // we should remove that in the future
+            GameState.SpriteAtlasManager.Blit(SomeObjectTileSheet, 0, 0);
+            GameState.SpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);;
+            GameState.SpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);
+            GameState.SpriteAtlasManager.Blit(SomeObjectTileSheet, 0, 0);
+            GameState.SpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);
+            GameState.SpriteAtlasManager.Blit(PlayerTileSheet, 0, 0);
+        }
+
+        // drawing the sprite atlas
+        void DrawSpriteAtlas()
+        {
+            SpriteAtlas.SpriteAtlas atlas = GameState.SpriteAtlasManager.GetSpriteAtlas(0);
+            DrawSprite(-3, -1, 
+                  atlas.Width / 32.0f, atlas.Height / 32.0f,
+                 atlas.Data, atlas.Width, atlas.Height);
+        }
+
+
+         // draws 1 sprite into the screen
+        // Note(Mahdi): this code is for testing purpose
+        void DrawSprite(float x, float y, float w, float h, byte[] spriteBytes,
+             int spriteW, int spriteH)
+        {
+            var tex = CreateTextureFromRGBA(spriteBytes, spriteW, spriteH);
+            var mat = Instantiate(Material);
+            mat.SetTexture("_MainTex", tex);
+            var mesh = CreateMesh(transform, "abc", 0, mat);
+
+            triangles.Clear();
+            uvs.Clear();
+            verticies.Clear();
+
+
+            var p0 = new Vector3(x, y, 0);
+            var p1 = new Vector3((x + w), (y + h), 0);
+            var p2 = p0; p2.y = p1.y;
+            var p3 = p1; p3.y = p0.y;
+
+            verticies.Add(p0);
+            verticies.Add(p1);
+            verticies.Add(p2);
+            verticies.Add(p3);
+
+            triangles.Add(0);
+            triangles.Add(2);
+            triangles.Add(1);
+            triangles.Add(0);
+            triangles.Add(1);
+            triangles.Add(3);
+
+            var u0 = 0;
+            var u1 = 1;
+            var v1 = -1;
+            var v0 = 0;
+
+            var uv0 = new Vector2(u0, v0);
+            var uv1 = new Vector2(u1, v1);
+            var uv2 = uv0; uv2.y = uv1.y;
+            var uv3 = uv1; uv3.y = uv0.y;
+
+
+            uvs.Add(uv0);
+            uvs.Add(uv1);
+            uvs.Add(uv2);
+            uvs.Add(uv3);
+    
+
+            mesh.SetVertices(verticies);
+            mesh.SetUVs(0, uvs);
+            mesh.SetTriangles(triangles, 0);
+        }
+
+
+        private Mesh CreateMesh(Transform parent, string name, int sortingOrder, Material material)
+        {
+            var go = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer));
+            go.transform.SetParent(parent);
+
+            var mesh = new Mesh
+            {
+                indexFormat = UnityEngine.Rendering.IndexFormat.UInt32
+            };
+
+            var mf = go.GetComponent<MeshFilter>();
+            mf.sharedMesh = mesh;
+            var mr = go.GetComponent<MeshRenderer>();
+            mr.sharedMaterial = material;
+            mr.sortingOrder = sortingOrder;
+
+            return mesh;
+        }
+
+        private void LateUpdate()
+        {
+        }
+
+        public struct R
+        {
+            public float X;
+            public float Y;
+            public float W;
+            public float H;
+
+            public R(float x, float y, float w, float h)
+            {
+                X = x;
+                Y = y;
+                W = w;
+                H = h;
+            }
+        }
+        private static R CalcVisibleRect()
+        {
+            var cam = Camera.main;
+            var pos = cam.transform.position;
+            var height = 2f * cam.orthographicSize;
+            var width = height * cam.aspect;
+            var visibleRect = new R(pos.x - width / 2, pos.y - height / 2, width, height);
+            return visibleRect;
+        }
+
+        // we use this helper function to generate a unity Texture2D
+        // from pixels
+        private Texture2D CreateTextureFromRGBA(byte[] rgba, int w, int h)
+        {
+
+            var res = new Texture2D(w, h, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point
+            };
+
+            var pixels = new Color32[w * h];
+            for (int x = 0 ; x < w; x++)
+            for (int y = 0 ; y < h; y++)
+            { 
+                int index = (x + y * w) * 4;
+                var r = rgba[index];
+                var g = rgba[index + 1];
+                var b = rgba[index + 2];
+                var a = rgba[index + 3];
+
+                pixels[x + y * w] = new Color32((byte)r, (byte)g, (byte)b, (byte)a);
+            }
+            res.SetPixels32(pixels);
+            res.Apply();
+
+            return res;
+        }
+        
+    }
+}
+
