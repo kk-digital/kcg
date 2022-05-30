825286c4c4242712b511942be150cae80245163b
Choi <ridvansu@hotmail.com>
2022-05-30 17:21:45 +0300

    Ticket #143: System Initialized
    Files created, initializations are done.

---
+++ Assets/src/agents/entities/VehicleEntity.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Components;
+
+namespace Entities
+{
+    public struct VehicleEntity
+    {
+        public int vehicleId;
+    }
+}
---
+++ Assets/src/agents/entities/VehicleList.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Entities;
+using Components;
+using Systems;
+
+public class VehicleList
+{
+    public VehicleEntity[] vehicleEntityList;
+    public VehiclePhysicsSystem[] vehiclePhysicsList;
+    public VehicleDrawSystem[] vehicleDrawList;
+    public VehicleComponentCollider[] vehicleComponentColliderList;
+    public VehicleComponentDraw[] vehicleComponentDrawList;
+}
