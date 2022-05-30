266de6f1209971615ebae93bdf26481e54ef093b
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-05-30 19:02:10 +0200

    ship weapons & projectiles classes

--- Assets/src/SystemView/ShipWeapon.cs
+++ Assets/src/SystemView/ShipWeapon.cs
@@ -1,18 +1,62 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine; // For color
 
 namespace SystemView
 {
     public class ShipWeapon
     {
+        public SystemShip Self;
+
         public Color ProjectileColor;
 
         public float Range;
 
         public float ShieldPenetration;
 
+        public float ProjectileVelocity;
+
         public int Damage;
 
         public int AttackSpeed; // in milliseconds
         public int Cooldown;    // in milliseconds
+
+        public List<ShipWeaponProjectile> ProjectilesFired;
+
+        public void TryFiring(SystemShip Target)
+        {
+            if (Cooldown > 0) return;
+
+            if (Self == Target) return;
+
+            if (Math.Sqrt(Self.PosX - Target.PosX) * (Self.PosX - Target.PosX) + (Self.PosY - Target.PosY) * (Self.PosY - Target.PosY) > Range) return;
+
+            Cooldown = AttackSpeed;
+
+            ShipWeaponProjectile Projectile = new ShipWeaponProjectile();
+
+            // todo: Projectile orbit
+
+            Projectile.Self = Self;
+
+            Projectile.PosX = Self.PosX;
+            Projectile.PosY = Self.PosY;
+
+            Projectile.Slope = (Target.PosX - Self.PosX) / (Target.PosY - Self.PosY);
+            Projectile.NegativeDirection = (Target.PosX - Self.PosX) < 0.0f;
+
+            Projectile.DistanceTravelled = 0.0f;
+            Projectile.Range = Range;
+
+            Projectile.ProjectileColor = ProjectileColor;
+
+            Projectile.ShieldPenetration = ShieldPenetration;
+
+            Projectile.ProjectileVelocity = ProjectileVelocity;
+
+            Projectile.Damage = Damage;
+
+            ProjectilesFired.Add(Projectile);
+        }
     }
 }
---
+++ Assets/src/SystemView/ShipWeaponProjectile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine; // For color
+
+namespace SystemView
+{
+    public class ShipWeaponProjectile
+    {
+        public OrbitingObjectDescriptor Descriptor;
+
+        public SystemShip Self;
+        public ShipWeapon Weapon;
+
+        public float PosX, PosY;
+
+        public float Slope;
+        public bool NegativeDirection;
+
+        public float DistanceTravelled;
+        public float Range;
+
+        public Color ProjectileColor;
+
+        public float ShieldPenetration;
+
+        public float ProjectileVelocity;
+
+        public int Damage;
+
+        public void UpdatePosition(float dt)
+        {
+            float d = dt * ProjectileVelocity;
+
+            if((DistanceTravelled += d) > Range)
+            {
+                Weapon.ProjectilesFired.Remove(this);
+                return;
+            }
+
+            // (1) dy = m * dx
+
+            // (2) dy^2 + dx^2 = d^2
+
+            //                                              d
+            // (3) (m^2 + 1) * dx^2 = d^2   =>   x = ± -----------
+            //                                         √ (m^2 + 1)
+
+            float dx = d / (float)Math.Sqrt(Slope * Slope + 1) * (NegativeDirection ? -1 : 1);
+
+            PosX += dx;
+            PosY += dx * Slope;
+        }
+    }
+}
