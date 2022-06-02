f6e008c352c185bd02ebc4e95408c8a20b461e93
Anton Zhukov <anton.zhukov@nanoreality.com>
2022-06-02 12:48:08 +0300

    Agent refactor

--- Assets/src/agents/components/Sprite2DComponent.cs
+++ Assets/src/agents/components/Sprite2DComponent.cs
@@ -1,4 +1,5 @@
 using Entitas;
+using SpriteAtlas;
 using UnityEngine;
 
 namespace Agent
@@ -12,66 +13,6 @@ namespace Agent
         public Vector2Int Size;
         public Material Material;
         
-        // TODO: ADD Mesh component
         public Mesh Mesh;
-
-        public void Init()
-        {
-            var atlasIndex = GameState.SpriteAtlasManager.CopySpriteToAtlas(SpriteID, 0, 0, 0);
-            
-            byte[] spriteBytes = new byte[Size.x * Size.y * 4];
-            GameState.SpriteAtlasManager.GetSpriteBytes(atlasIndex, spriteBytes);
-            var mat = UnityEngine.Object.Instantiate(Material);
-            var tex = CreateTextureFromRGBA(spriteBytes, Size.x, Size.y);
-            mat.SetTexture("_MainTex", tex);
-           
-        }
-        
-        private Texture2D CreateTextureFromRGBA(byte[] rgba, int w, int h)
-        {
-
-            var res = new Texture2D(w, h, TextureFormat.RGBA32, false)
-            {
-                filterMode = FilterMode.Point
-            };
-
-            var pixels = new Color32[w * h];
-            for (int x = 0; x < w; x++)
-            {
-                for (int y = 0; y < h; y++)
-                {
-                    int index = (x + y * w) * 4;
-                    var r = rgba[index];
-                    var g = rgba[index + 1];
-                    var b = rgba[index + 2];
-                    var a = rgba[index + 3];
-
-                    pixels[x + y * w] = new Color32(r, g, b, a);
-                }
-            }
-
-            res.SetPixels32(pixels);
-            res.Apply();
-
-            return res;
-        }
-        
-        private Mesh CreateMesh(string name, int sortingOrder, Material material)
-        {
-            var go = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer));
-
-            var mesh = new Mesh
-            {
-                indexFormat = UnityEngine.Rendering.IndexFormat.UInt32
-            };
-
-            var mf = go.GetComponent<MeshFilter>();
-            mf.sharedMesh = mesh;
-            var mr = go.GetComponent<MeshRenderer>();
-            mr.sharedMaterial = material;
-            mr.sortingOrder = sortingOrder;
-
-            return mesh;
-        }
     }
 }
--- Assets/src/agents/systems/AgentSpawnerSystem.cs
+++ Assets/src/agents/systems/AgentSpawnerSystem.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+using SpriteAtlas;
 using UnityEngine;
 
 namespace Agent
@@ -31,12 +33,71 @@ namespace Agent
             var spritePath = "Assets\\StreamingAssets\\Moonbunker\\Tilesets\\Sprites\\character\\character.png";
             var spriteID = GameState.SpriteLoader.GetSpriteSheetID(spritePath, spriteSize.x, spriteSize.y);
 
-            entity.AddSprite2D(spriteID, spritePath, spriteSize, material, null);
-            entity.sprite2D.Init();
+            entity.AddSprite2D(spriteID, spritePath, spriteSize, material, BuildMesh(spriteID, material, spriteSize));
 
             entity.AddPosition2D(new Vector2(3f, 2f), default);
 
             return entity;
+        }
+        
+        private Mesh BuildMesh(int spriteID, Material material, Vector2Int spriteSize)
+        {
+            var atlasIndex = GameState.SpriteAtlasManager.CopySpriteToAtlas(spriteID, 0, 0, AtlasType.Agent);
+            
+            byte[] spriteBytes = new byte[spriteSize.x * spriteSize.y * 4];
+            GameState.SpriteAtlasManager.GetSpriteBytes(atlasIndex, spriteBytes, AtlasType.Agent);
+            var mat = UnityEngine.Object.Instantiate(material);
+            var tex = CreateTextureFromRGBA(spriteBytes, spriteSize.x, spriteSize.y);
+            mat.SetTexture("_MainTex", tex);
+
+            return InstantiateMesh("Agent", 0, mat);
+        }
+        
+        private Texture2D CreateTextureFromRGBA(byte[] rgba, int w, int h)
+        {
+
+            var res = new Texture2D(w, h, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point
+            };
+
+            var pixels = new Color32[w * h];
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    int index = (x + y * w) * 4;
+                    var r = rgba[index];
+                    var g = rgba[index + 1];
+                    var b = rgba[index + 2];
+                    var a = rgba[index + 3];
+
+                    pixels[x + y * w] = new Color32(r, g, b, a);
+                }
+            }
+
+            res.SetPixels32(pixels);
+            res.Apply();
+
+            return res;
+        }
+        
+        private Mesh InstantiateMesh(string name, int sortingOrder, Material material)
+        {
+            var go = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer));
+
+            var mesh = new Mesh
+            {
+                indexFormat = UnityEngine.Rendering.IndexFormat.UInt32
+            };
+
+            var mf = go.GetComponent<MeshFilter>();
+            mf.sharedMesh = mesh;
+            var mr = go.GetComponent<MeshRenderer>();
+            mr.sharedMaterial = material;
+            mr.sortingOrder = sortingOrder;
+
+            return mesh;
         }
     }
 }
