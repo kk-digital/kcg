eadf29ecff7e30558bdc9ffba9e51b44feaf014b
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-06-03 14:51:42 +0200

    Ship controls

--- Assets/Scripts/SystemView/PlayerShip.cs
+++ Assets/Scripts/SystemView/PlayerShip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,8 +12,18 @@ namespace SystemView
         public GameObject Object;
         public SystemShipRenderer Renderer;
 
+        public float LastTime;
+
+        public float rotation = 0.0f;
+
+        public float RotationSpeedModifier = 1.0f;
+
+        public bool Reverse = false;
+
         private void Start()
         {
+            LastTime = Time.time * 1000.0f;
+
             Ship = new SystemShip();
 
             Object = new GameObject();
@@ -35,6 +46,42 @@ namespace SystemView
 
         private void Update()
         {
+            float CurrentTime = Time.time - LastTime;
+
+            if (CurrentTime == 0.0f) return;
+
+            LastTime = Time.time;
+
+            Ship.Rotation -= Input.GetAxis("Horizontal") * CurrentTime * RotationSpeedModifier;
+
+            if (Input.GetAxis("Vertical") > 0.0f && Ship.VelX * Ship.VelX + Ship.VelY * Ship.VelY < 0.1f) Reverse = false;
+            if (Input.GetAxis("Vertical") < 0.0f && Ship.VelX * Ship.VelX + Ship.VelY * Ship.VelY < 0.1f) Reverse = true;
+
+            float AccX = (float)Math.Cos(Ship.Rotation);
+            float AccY = (float)Math.Sin(Ship.Rotation);
+
+            float Magnitude = (float)Math.Sqrt(AccX * AccX + AccY * AccY);
+
+            AccX = AccX / Magnitude * Ship.Acceleration * CurrentTime * Input.GetAxis("Vertical");
+            AccY = AccY / Magnitude * Ship.Acceleration * CurrentTime * Input.GetAxis("Vertical");
+
+            Ship.PosX += Ship.VelX * CurrentTime + AccX / 2.0f * CurrentTime * CurrentTime;
+            Ship.PosY += Ship.VelY * CurrentTime + AccY / 2.0f * CurrentTime * CurrentTime;
+
+            Ship.VelX += AccX * CurrentTime;
+            Ship.VelY += AccY * CurrentTime;
+
+            // "Air resistance" effect
+            Ship.VelX *= 0.995f;
+            Ship.VelY *= 0.995f;
+
+            // "Sailing" effect
+            Magnitude = (float)Math.Sqrt(Ship.VelX * Ship.VelX + Ship.VelY * Ship.VelY);
+
+            Ship.VelX = (3.0f * Ship.VelX + (float)Math.Cos(Ship.Rotation) * Magnitude * (Reverse ? -1.0f : 1.0f)) / 4.0f;
+            Ship.VelY = (3.0f * Ship.VelY + (float)Math.Sin(Ship.Rotation) * Magnitude * (Reverse ? -1.0f : 1.0f)) / 4.0f;
+
+            Renderer.shipColor.b = Ship.Health / Ship.MaxHealth;
         }
     }
 }
