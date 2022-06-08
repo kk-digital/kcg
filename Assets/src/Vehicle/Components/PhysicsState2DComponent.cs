6f9db7f78b4d5deb4412cb5ef430831b258807cc
Choi <ridvansu@hotmail.com>
2022-06-08 15:15:00 +0300

    Vehicle Y Movement and Gravity Added Issue #143
    Vehicle Y Movement and Gravity Added Issue #143 .

--- Assets/src/Vehicle/Components/PhysicsState2DComponent.cs
+++ Assets/src/Vehicle/Components/PhysicsState2DComponent.cs
@@ -13,11 +13,13 @@ namespace Vehicle
         public Vector2 Scale = Vector2.zero;
         public Vector2 TempScale = Vector2.zero;
 
+        [Range(-1.0f, 1.0f)]
         public Vector2 angularVelocity = Vector2.zero;
+
         public float angularMass = 1.0f;
         public float angularAcceleration = 3.0f;
         
-        public float centerOfGravity = 1.0f;
+        public float centerOfGravity = 0.0f;
         public Vector2 centerOfRotation = Vector2.one;
     }
 }
--- Assets/src/Vehicle/Systems/ProcessVelocitySystem.cs
+++ Assets/src/Vehicle/Systems/ProcessVelocitySystem.cs
@@ -45,8 +45,7 @@ namespace Vehicle
             }
         }
 
-        // Move X Function 
-        public void ProcessMovementX(float speed, bool positiveAxis, Contexts contexts)
+        public void ProcessMovement(Vector2 newSpeed, Contexts contexts)
         {
             // Get Vehicle Entites
             IGroup<GameEntity> entities =
@@ -57,26 +56,71 @@ namespace Vehicle
                 var position = vehicle.vehiclePhysicsState2D;
                 position.TempPosition = position.Position;
 
-                // Read the input axis and accelerate the vehicle
-                if(positiveAxis)
-                {
-                    vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
-                        new Vector2(speed, 0.0f), vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
+
+                // Update the position
+                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
+                    newSpeed, vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
                          vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
-                }
-                else
-                {
-                    vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
-                        new Vector2(-speed, 0.0f), vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
-                         vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
-                }
 
                 // Add velocity to position
                 position.Position += vehicle.vehiclePhysicsState2D.angularVelocity * Time.deltaTime;
 
                 // Update the position
-                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale, 
-                    position.angularVelocity, vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
+                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
+                    vehicle.vehiclePhysicsState2D.angularVelocity, vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
+                         vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
+
+                return;
+
+            }
+        }
+
+        public void UpdateGravity(Contexts contexts)
+        {
+            // Get Vehicle Entites
+            IGroup<GameEntity> entities =
+            contexts.game.GetGroup(GameMatcher.VehiclePhysicsState2D);
+            foreach (var vehicle in entities)
+            {
+                if (vehicle.vehiclePhysicsState2D.angularVelocity.y > 0.5f)
+                    return;
+
+                // Get position from component
+                var position = vehicle.vehiclePhysicsState2D;
+                position.TempPosition = position.Position;
+
+                // Update the position
+                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
+                    new Vector2(vehicle.vehiclePhysicsState2D.angularVelocity.x, (vehicle.vehiclePhysicsState2D.angularVelocity.y - 1.5f) * Time.deltaTime), vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
+                         vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
+
+                // Add velocity to position
+                position.Position += vehicle.vehiclePhysicsState2D.angularVelocity * Time.deltaTime;
+
+                // Update the position
+                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
+                    vehicle.vehiclePhysicsState2D.angularVelocity, vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
+                         vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
+
+                return;
+            }
+        }
+
+        public void StopMovement(Vector2 newSpeed, Contexts contexts)
+        {
+            // Update the component
+            // Get Vehicle Entites
+            IGroup<GameEntity> entities =
+            contexts.game.GetGroup(GameMatcher.VehiclePhysicsState2D);
+            foreach (var vehicle in entities)
+            {
+                // Get position from component
+                var position = vehicle.vehiclePhysicsState2D;
+                position.TempPosition = position.Position;
+
+                // Update the position
+                vehicle.ReplaceVehiclePhysicsState2D(position.Position, position.TempPosition, position.Scale, position.TempScale,
+                    newSpeed, vehicle.vehiclePhysicsState2D.angularMass, vehicle.vehiclePhysicsState2D.angularAcceleration,
                          vehicle.vehiclePhysicsState2D.centerOfGravity, vehicle.vehiclePhysicsState2D.centerOfRotation);
             }
         }
