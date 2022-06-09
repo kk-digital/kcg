eb124ed296bdf4b7b1e627f9af2314160a5438bf
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-06-09 15:54:53 +0200

    Player ship now shows which direction it is facing

--- Assets/Scripts/SystemView/SystemShipRenderer.cs
+++ Assets/Scripts/SystemView/SystemShipRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,10 +12,12 @@ namespace SystemView
         public SpriteRenderer ShipRender;
         public SpriteRenderer ShieldRender;
         public OrbitRenderer OrbitRender;
+        public LineRenderer DirectionRenderer;
 
-        public Color orbitColor = new Color(1.0f, 0.7f, 0.5f, 1.0f);
-        public Color shieldColor = new Color(0.4f, 0.7f, 1.0f, 0.5f);
-        public Color shipColor = Color.white;
+        public Color orbitColor     = new Color(1.0f, 0.7f, 0.5f, 1.0f);
+        public Color shieldColor    = new Color(0.4f, 0.7f, 1.0f, 0.5f);
+        public Color directionColor = new Color(1.0f, 0.7f, 0.5f, 0.4f);
+        public Color shipColor      = Color.white;
 
         public float width = 1.0f;
 
@@ -42,6 +45,25 @@ namespace SystemView
             // Temporary sprites
             ShipRender.sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
             ShieldRender.sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Knob.psd");
+
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            Material mat = new Material(shader);
+            mat.hideFlags = HideFlags.HideAndDontSave;
+
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+
+            // Turn off backface culling, depth writes, depth test.
+            mat.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            mat.SetInt("_ZWrite", 0);
+            mat.SetInt("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
+
+            GameObject DirectionObject = new GameObject();
+            DirectionObject.name = "Ship direction renderer";
+
+            DirectionRenderer = DirectionObject.AddComponent<LineRenderer>();
+            DirectionRenderer.material = mat;
+            DirectionRenderer.useWorldSpace = true;
         }
 
         // Update is called once per frame
@@ -65,6 +87,21 @@ namespace SystemView
                 ShieldRender.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
             else
                 ShieldRender.color = new Color(shieldColor.r, shieldColor.g, shieldColor.b, shieldColor.a * ship.Shield / ship.MaxShield);
+
+            if (ship.Weapons.Count > 0 && ship.Self.VelX * ship.Self.VelX + ship.Self.VelY * ship.Self.VelY > 0.0f)
+            {
+                Vector3[] vertices = new Vector3[2];
+                vertices[0] = new Vector3(ship.Self.PosX, ship.Self.PosY, -0.075f);
+                vertices[1] = new Vector3(ship.Self.PosX + (float)Math.Cos(ship.Rotation) * 10.0f / Camera.scale, ship.Self.PosY + (float)Math.Sin(ship.Rotation) * 10.0f / Camera.scale, -0.075f);
+                DirectionRenderer.SetPositions(vertices);
+                DirectionRenderer.positionCount = 2;
+                DirectionRenderer.startColor = DirectionRenderer.endColor = directionColor;
+                DirectionRenderer.startWidth = DirectionRenderer.endWidth = 0.2f / Camera.scale;
+            }
+            else
+            {
+                DirectionRenderer.positionCount = 0;
+            }
 
             if (!ship.PathPlanned) OrbitRender.descriptor = null;
             else OrbitRender.descriptor = ship.Descriptor;
