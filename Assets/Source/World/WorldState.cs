901dd64b33aff5a4b976135cc4672d5f6cfdff3b
Choi <ridvansu@hotmail.com>
2022-06-09 15:52:16 +0300

    Camera Enums Added Issue #207
    Camera Enums Added Issue #207

---
+++ Assets/Source/World/WorldState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using SystemView;
+using Planet;
+
+namespace World
+{
+    public struct WorldState
+    {
+        SystemPlanet[] systemPlanets;
+    }
+}
