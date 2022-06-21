aed47e663c853f59f46e95162433d256b64dc80d
Anton Zhukov <anton.zhukov@nanoreality.com>
2022-06-21 12:23:01 +0300

    Returned Methode GetChunkIndex. Convert chunklist from 2d array to 1d array

--- Assets/Source/Planet/ChunkList.cs
+++ Assets/Source/Planet/ChunkList.cs
@@ -7,68 +7,35 @@ namespace Planet
 {
     public struct ChunkList
     {
-        private Chunk[][] data;
-        public int ChunkCount => data.Sum(yChunks => yChunks.Length);
+        private Chunk[] data;
+        private readonly int mapSizeX;
 
         public ChunkList(Vec2i mapSize)
         {
             // xCount & 0x0f == xCount AND 15
             // (>> 4) == (/ 16)
-
-            var xCount = mapSize.X >> 4;
-            if ((mapSize.X & 0x0f) != 0)
-                xCount++;
             
-            var yCount = mapSize.Y >> 4;
-            if ((mapSize.Y & 0x0f) != 0)
-                yCount++;
+            mapSizeX = mapSize.X;
+            var firstChunkCount = 1;
 
-            data = new Chunk[yCount][];
-            
-            for (int y = 0; y < yCount; y++)
-            {
-                data[y] = Enumerable.Repeat(new Chunk(Enums.Tile.MapChunkType.Empty), xCount).ToArray();
-            }
+            data = Enumerable.Repeat(new Chunk(Enums.Tile.MapChunkType.Empty), firstChunkCount).ToArray();
         }
-
-        public int GetXAxisChunksCount(int y)
-        {
-            return data[y].Length;
-        }
-
-        public int GetYAxisChunksCount(int x)
-        {
-            var count = 0;
-
-            for (int y = 0; y < data.Length; y++)
-            {
-                if (GetXAxisChunksCount(y) > x)
-                {
-                    count++;
-                }
-            }
-
-            return count;
-        }
-        
         
         [MethodImpl((MethodImplOptions) 256)]
-        public ref Chunk GetChunkRef(int tileX, int tileY)
-        {
-            // (>> 4) == (/ 16)
-            return ref data[tileX >> 4][tileY >> 4];
-        }
+        // (<< 4) == (* 16)
+        public int GetChunkIndex(int x, int y) => ((x << 4) + y * mapSizeX) >> 8;
+        public ref Chunk GetChunkRef(int tileX, int tileY) => ref data[GetChunkIndex(tileX, tileY)];
         
-        public void AddChunkOnX(int y, int count = 1)
+        public void AddChunk(int count = 1)
         {
-            var oldChunkCountOnX = data[y].Length;
+            var oldChunkCountOnX = data.Length;
             var newChunkCountOnX = oldChunkCountOnX + count;
             
-            Array.Resize(ref data[y], newChunkCountOnX);
+            Array.Resize(ref data, newChunkCountOnX);
 
-            for (int x = oldChunkCountOnX; x < newChunkCountOnX; x++)
+            for (int i = oldChunkCountOnX; i < newChunkCountOnX; i++)
             {
-                data[y][x] = new Chunk(Enums.Tile.MapChunkType.Empty);
+                data[i] = new Chunk(Enums.Tile.MapChunkType.Empty);
             }
         }
     }
