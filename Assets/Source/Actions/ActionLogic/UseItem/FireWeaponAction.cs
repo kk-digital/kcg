c725141dbe7178c13829f6009dec65a606cdc09c
João Marcos Mororo Costa <joao_marcos_8@hotmail.com>
2022-06-22 16:13:54 -0300

    Create basic FireWeaponAction

---
+++ Assets/Source/Actions/ActionLogic/UseItem/FireWeaponAction.cs
@@ -0,0 +1,30 @@
+﻿
+using KMath;
+using System.Web.UI.WebControls;
+using UnityEngine;
+
+namespace Action
+{
+    public class FireWeaponAction : ActionBase
+    {
+        public FireWeaponAction(int actionID, int agentID) : base(actionID, agentID)
+        {
+
+        }
+
+        public override void OnEnter()
+        {
+            
+        }
+    }
+
+    // Factory Method
+    public class FireWeaponActionCreator : ActionCreator
+    {
+        public override ActionBase CreateAction(int actionID, int agentID)
+        {
+            return new MiningLaserToolAction(actionID, agentID);
+        }
+    }
+}
+
