2cf12ef103e0781567c6282ea6096f3a01e68fe0
devcore6 <61387985+devcore6@users.noreply.github.com>
2022-06-29 14:34:21 +0200

    Station health, shields, and docking

--- Assets/Source/SystemView/SpaceStation.cs
+++ Assets/Source/SystemView/SpaceStation.cs
@@ -9,8 +9,24 @@ namespace Source {
             public SpaceObject              Self        = new();
             public OrbitingObjectDescriptor descriptor;
 
+            public int health, max_health;
+            public int shield, max_shield;
+
+            public int shield_regeneration_rate;
+
             public SpaceStation() {
                 descriptor = new(Self);
+            }
+
+            public void dock(SystemShip ship) {
+                // todo: check if ship has permission to dock? or if docking bay is full?
+                //       should docking bay have a maximum capacity?
+
+                LandedShips.Add(ship);
+            }
+
+            public void undock(SystemShip ship) {
+                LandedShips.Remove(ship);
             }
         }
     }
