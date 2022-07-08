4e571e783e52608422cce554949bbc2ecc518ad3
Anton Zhukov <anton.zhukov@nanoreality.com>
2022-07-08 16:41:05 +0300

    ref 1 implementation alghoritm bug rewrite

--- Assets/Source/Collisions/RectScanTilesRef1.cs
+++ Assets/Source/Collisions/RectScanTilesRef1.cs
@@ -11,70 +11,52 @@ namespace Collisions
     {
         public static Vec2i[] RectScan(Vec2f center, Vec2f halfSize, Vec2f velocity)
         {
-            var previousRightX = (int) Math.Ceiling(center.X + halfSize.X); //round up
-            var currentRightX = (int) Math.Ceiling(center.X + halfSize.X + velocity.X); //round up
+            var previous_box_xmax = (int) Math.Ceiling(center.X + halfSize.X); //round up
+            var current_box_xmax = (int) Math.Ceiling(center.X + halfSize.X + velocity.X); //round up
 
-            var previousLeftX = (int) Math.Floor(center.X - halfSize.X); //round down
-            var currentLeftX = (int) Math.Floor(center.X - halfSize.X + velocity.X); //round down
+            var previous_box_xmin = (int) Math.Floor(center.X - halfSize.X); //round down
+            var current_box_xmin = (int) Math.Floor(center.X - halfSize.X + velocity.X); //round down
 
-            var previousTopY = (int) Math.Ceiling(center.Y + halfSize.Y); //round up
-            var currentTopY = (int) Math.Ceiling(center.Y + halfSize.Y + velocity.Y); //round up
+            var previous_box_ymax = (int) Math.Ceiling(center.Y + halfSize.Y); //round up
+            var current_box_ymax = (int) Math.Ceiling(center.Y + halfSize.Y + velocity.Y); //round up
 
-            var previousBottomY = (int) Math.Floor(center.Y - halfSize.Y); //round down
-            var currentBottomY = (int) Math.Floor(center.Y - halfSize.Y + velocity.Y); //round down
+            var previous_box_ymin = (int) Math.Floor(center.Y - halfSize.Y); //round down
+            var current_box_ymin = (int) Math.Floor(center.Y - halfSize.Y + velocity.Y); //round down
 
             //verify, that no tile is checked twice
             //do a count to verify
             //NOTE: Do asserts, that min<=max and swap if not
 
-            int xmin_1 = velocity.X >= 0f ? previousRightX : currentLeftX;
-            int xmax_1 = velocity.X >= 0f ? currentRightX : previousLeftX;
-            int ymin_1 = velocity.Y >= 0f ? previousTopY : currentBottomY;
-            int ymax_1 = velocity.Y >= 0f ? currentTopY : previousBottomY;
+            int xmin_1 = velocity.X >= 0f ? previous_box_xmin : current_box_xmin;
+            int xmax_1 = velocity.X >= 0f ? current_box_xmax  : previous_box_xmax;
+            int ymin_1 = velocity.Y >= 0f ? previous_box_ymax : current_box_ymin;
+            int ymax_1 = velocity.Y >= 0f ? current_box_ymax  : previous_box_ymin;
             
-            int xmin_2 = Math.Min(currentLeftX, previousLeftX);
-            int xmax_2 = Math.Max(previousRightX, currentRightX);
-            int ymin_2 = Math.Min(previousBottomY, currentBottomY);
-            int ymax_2 = Math.Max(previousTopY, currentTopY);
+            int xmin_2 = velocity.X >= 0f ? previous_box_xmax : current_box_xmin;
+            int xmax_2 = velocity.X >= 0f ? current_box_xmax  : previous_box_xmin;
+            int ymin_2 = velocity.Y >= 0f ? previous_box_ymin : current_box_ymin;
+            int ymax_2 = velocity.Y >= 0f ? current_box_ymax : previous_box_ymax;
 
-            int outputCount = (currentTopY - currentBottomY) * (xmax_1 - xmin_1);
-            outputCount += (ymax_1 - ymin_1) * (currentRightX - currentLeftX);
-            outputCount += (currentTopY - currentBottomY) * (xmax_2 - xmin_2);
-            outputCount += (ymax_2 - ymin_1) * (currentRightX - currentLeftX);
+            int outputCount = (xmin_1 - xmax_1) * (ymin_1 - ymax_1);
+            outputCount += (xmin_2 - xmax_2) * (ymin_2 - ymax_2);
 
             var output = new Vec2i[outputCount];
             var outputIndex = 0;
 
-            for (int y = currentBottomY; y <= currentTopY; y++)
+            for (int x = xmin_1; x <= xmax_1; x++)
             {
-                for (int x = xmin_1; x <= xmax_1; x++)
+                for (int y = ymin_1; y <= ymax_1; y++)
                 {
                     output[outputIndex] = new Vec2i(x, y);
                     outputIndex++;
                 }
             }
             
-            for (int y = ymin_1; y <= ymax_1; y++)
-            {
-                for (int x = currentLeftX; x <= currentRightX; x++)
-                {
-                    output[outputIndex] = new Vec2i(x, y);
-                    outputIndex++;
-                }
-            }
+
             
-            for (int y = currentBottomY; y <= currentTopY; y++)
+            for (int y = xmin_2; y <= xmax_2; y++)
             {
-                for (int x = xmin_2; x <= xmax_2; x++)
-                {
-                    output[outputIndex] = new Vec2i(x, y);
-                    outputIndex++;
-                }
-            }
-            
-            for (int y = ymin_2; y <= ymax_2; y++)
-            {
-                for (int x = currentLeftX; x <= currentRightX; x++)
+                for (int x = ymin_2; x <= ymax_2; x++)
                 {
                     output[outputIndex] = new Vec2i(x, y);
                     outputIndex++;
