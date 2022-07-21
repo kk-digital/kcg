d5ef779f4eaa4b9659cc3709e091cba85c8f8f2d
CatWithPaws <valentunkulakov6@gmail.com>
2022-07-21 12:56:47 +0300

    Added Platform collision behaviour

--- Assets/Source/Collisions/Collisions.cs
+++ Assets/Source/Collisions/Collisions.cs
@@ -104,6 +104,10 @@ namespace Collisions
                     if (x >= 0 && x < tileMap.MapSize.X)
                     {
                         ref var tile = ref tileMap.GetFrontTile(x, y);
+                        if (GameState.TileCreationApi.GetTileProperty(tile.ID).CollisionIsoType == CollisionType.Platform)
+                        {
+                            return false;
+                        }
                         if (tile.ID != TileID.Air)
                         {
                             var tileBorders = new AABox2D(x, y);
