3455dca30d62719fd1c0c5ae37210ed1ebc22b99
Choi <ridvansu@hotmail.com>
2022-07-22 15:02:18 +0300

    Comments Added
    Comments Added

--- Assets/Source/GuiHud/GUI/Elements/ProgressBar.cs
+++ Assets/Source/GuiHud/GUI/Elements/ProgressBar.cs
@@ -1,56 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace KGUI.Elements
 {
     public class ProgressBar
     {
+        // Bar Gameobject
         private GameObject Bar;
+
+        // Bar Fill Value
         public float _fillValue;
 
+        // Constructor
         public ProgressBar(string barName, Transform parent, Sprite barTexture, UnityEngine.UI.Image.FillMethod fillMethod, float fillValue, AgentEntity agentEntity)
         {
+            // Set Fill Value
             _fillValue = fillValue;
 
-            // Bar Initializon
+            // Create Gameobject
             Bar = new GameObject(barName);
+
+            // Set Parent
             Bar.transform.parent = parent;
+
+            // Add Rect Transform Component
             Bar.AddComponent<RectTransform>();
+
+            // Add Image Component
             Bar.AddComponent<UnityEngine.UI.Image>();
+
+            // Set Anchor Min
             Bar.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
+
+            // Set Anchor Max
             Bar.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
+
+            // Set Pivot
             Bar.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
 
+            // Set Sprite
             Bar.GetComponent<UnityEngine.UI.Image>().sprite = barTexture;
+
+            // Set Raycast Target
             Bar.GetComponent<UnityEngine.UI.Image>().raycastTarget = true;
+
+            // Set Maskable
             Bar.GetComponent<UnityEngine.UI.Image>().maskable = true;
+
+            // Set Image Type
             Bar.GetComponent<UnityEngine.UI.Image>().type = UnityEngine.UI.Image.Type.Filled;
+
+            // Set Fill Method
             Bar.GetComponent<UnityEngine.UI.Image>().fillMethod = fillMethod;
+
+            // Set Fill Origin
             Bar.GetComponent<UnityEngine.UI.Image>().fillOrigin = 0;
+
+            // Set Fill Value
             Bar.GetComponent<UnityEngine.UI.Image>().fillAmount = fillValue;
+
+            // Set Fil Clockwise
             Bar.GetComponent<UnityEngine.UI.Image>().fillClockwise = true;
         }
 
         public void SetPosition(Vector3 newPos)
         {
+            // Set Position
             Bar.GetComponent<RectTransform>().localPosition = newPos;
         }
 
         public void SetRotation(Quaternion newRot)
         {
+            // Set Rotation
             Bar.GetComponent<RectTransform>().localRotation = newRot;
         }
 
         public void SetScale(Vector3 newScale)
         {
+            // Set Scale
             Bar.GetComponent<RectTransform>().localScale = newScale;
         }
 
         public void Update(float fillValue)
         {
+            // Update Fill Value
             _fillValue = fillValue;
+
+            // Update Fill Value
             Bar.GetComponent<UnityEngine.UI.Image>().fillAmount = fillValue;
         }
     }
--- Assets/Source/GuiHud/GUI/GUIManager.cs
+++ Assets/Source/GuiHud/GUI/GUIManager.cs
@@ -7,10 +7,19 @@ namespace KGUI
 {
     public class GUIManager
     {
+        // Food Bar
         static KGUI.PlayerStatus.FoodBarUI foodBarUI = new PlayerStatus.FoodBarUI();
+
+        // Water Bar
         static KGUI.PlayerStatus.WaterBarUI waterBarUI = new PlayerStatus.WaterBarUI();
+
+        // Oxygen Bar
         static KGUI.PlayerStatus.OxygenBarUI oxygenBarUI = new PlayerStatus.OxygenBarUI();
+
+        // Fuel Bar
         static KGUI.PlayerStatus.FuelBarUI fuelBarUI = new PlayerStatus.FuelBarUI();
+
+        // Health Bar
         static KGUI.PlayerStatus.HealthBarUI healthBarUI = new PlayerStatus.HealthBarUI();
 
         // GUI Elements List
@@ -25,14 +34,22 @@ namespace KGUI
         // Run Various Functions One Time
         private bool CanRun = true;
 
+        // Initialize
         public virtual void Initialize(Contexts contexts, AgentEntity agentEntity)
         {
-            // Add Elements
+            // Add Food Bar To Draw Array
             UIList.Add(foodBarUI);
+
+            // Add Water Bar To Draw Array
             UIList.Add(waterBarUI);
+
+            // Add Oxygen Bar To Draw Array
             UIList.Add(oxygenBarUI);
+
+            // Add Fuel Bar To Draw Array
             UIList.Add(fuelBarUI);
 
+            // Add Health Bar To Draw Array
             healthBarUI.Initialize(agentEntity);
 
             // Init Elements
@@ -52,18 +69,26 @@ namespace KGUI
 
             // Assign New Cursor Position
             CursorPosition = new Vec2f(Input.mousePosition.x, Input.mousePosition.y);
+
+            // Draw Health Bar
             healthBarUI.Draw(agentEntity);
+
+            // Handle Inputs
             HandleInputs(agentEntity);
         }
 
         public void HandleInputs(AgentEntity agentEntity)
         {
+            // On Mouse Click Event
             OnMouseClick(agentEntity);
+
+            // On Mouse Stay Event
             OnMouseStay();
         }
 
         public virtual void OnMouseClick(AgentEntity agentEntity)
         {
+            // If Mosue 0 Button Down
             if (Input.GetMouseButtonDown(0))
             {
                 // Handle Inputs
@@ -85,11 +110,16 @@ namespace KGUI
             // Handle Inputs
             for (int i = 0; i < UIList.Count; i++)
             {
+                // If Condition Is True
                 if(UIList[i].CanRun)
                 {
+                    // Set Condition Bool
                     UIList[i].CanRun = false;
+
+                    // If Cursor Is In UI Element
                     if (Vector2.Distance(new Vector2(CursorPosition.X, CursorPosition.Y), new Vector2(UIList[i].ObjectPosition.X, UIList[i].ObjectPosition.Y)) < 20.0f)
                     {
+                        // Run Mouse Enter Event
                         UIList[i].OnMouseEnter();
                     }
                 }
@@ -101,16 +131,20 @@ namespace KGUI
             // Handle Inputs
             for (int i = 0; i < UIList.Count; i++)
             {
+                // Check If Cursor Is On UI Element
                 if (Vector2.Distance(new Vector2(CursorPosition.X, CursorPosition.Y), new Vector2(UIList[i].ObjectPosition.X, UIList[i].ObjectPosition.Y)) < 20.0f)
                 {
+                    // Run Mouse Enter Event
                     OnMouseEnter();
+
+                    // Call Mouse Stay Event For Each
                     UIList[i].OnMouseStay();
                 }
                 else if (Vector2.Distance(new Vector2(CursorPosition.X, CursorPosition.Y), new Vector2(UIList[i].ObjectPosition.X, UIList[i].ObjectPosition.Y)) > 30.0f)
                 {
+                    // Reset When Out Of the UI Element
                     UIList[i].CanRun = true;
                 }
-
             }
         }
 
@@ -119,11 +153,16 @@ namespace KGUI
             // Handle Inputs
             for (int i = 0; i < UIList.Count; i++)
             {
+                // If Condition Is False
                 if (!UIList[i].CanRun)
                 {
+                    // Set Condition to True
                     UIList[i].CanRun = true;
+
+                    // If Cursor is Out of UI Element
                     if (Vector2.Distance(new Vector2(CursorPosition.X, CursorPosition.Y), new Vector2(UIList[i].ObjectPosition.X, UIList[i].ObjectPosition.Y)) > 30.0f)
                     {
+                        // On Mouse Exit
                         UIList[i].OnMouseExit();
                     }
                 }
